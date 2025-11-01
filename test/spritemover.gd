ca419df48f08274d018218c0caef5552ed574b8a
Wikki <vfhjort@gmail.com>
2025-11-01 17:11:54 +0100

    Added icon tornado

--- test/spritemover.gd
+++ test/spritemover.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+var speed: Vector2 = Vector2(10, 20)
+@export var change:= false
+var rotation_value: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position.x = randf_range(0, 1152)
 	position.y = randf_range(0, 648)
 
-var speed: Vector2 = Vector2(10, 20)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if position.y > 648:
@@ -18,3 +20,7 @@ func _process(delta: float) -> void:
 	if position.x < 0:
 		speed.x *= -1
 	position += speed*delta
+	rotation += delta*rotation_value
+	if change:
+		rotation_value = randi_range(1, 5)*((randi()%2)*2-1)
+		change = false
